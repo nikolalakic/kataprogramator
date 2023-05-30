67d97573767417ed02a536dc76989ef649e6d230
Nikola Lakic <nlakic@pm.me>
2023-05-30 20:47:19 +0200

    Prvi komit da kata vidi kako radi

---
+++ prvikod.py
@@ -0,0 +1,5 @@
+a = float(input("Unesi stranicu trougla a: "))
+b = float(input("Unesi stranicu trougla b: "))
+c = "kurac"
+povrsina_trougla = a*b/2
+print("Povrsina trougla stanica a=3 i b=2 je: ", str(povrsina_trougla) + c)
