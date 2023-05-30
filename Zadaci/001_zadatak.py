33522ea21037281ada8b056dd2947f6a84a062b1
Nikola Lakic <nlakic@pm.me>
2023-05-30 22:08:07 +0200

    Prvi pravi zadatak za katu

--- Zadaci/001_zadatak.py
+++ Zadaci/001_zadatak.py
@@ -1,1 +1,3 @@
-#Napisati kod koji proverata da li je uneto ime "Kata Vukasinovic" tacno.
+# TODO
+# Napisati skriptu koja za uneti input (broj) vraca koren tog broja ako je broj deljiv sa 5,
+# a ako nije deljiv sa 5 onda da vrati ostatak tog broja. Npr ako uneses broj 10 treba da stampa 3.16 kao koren
